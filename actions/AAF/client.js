e2b1813778d80ad9c070e8ef4c7656a58875ec25
samuel ogundipe <ayoogundipe2005@gmail.com>
2019-03-07 18:04:55 +0100

    Automatic commit from Bubble

--- actions/AAF/client.js
+++ actions/AAF/client.js
@@ -1,19 +1,25 @@
 function(properties, context) {
-  //Load any data 
+    //Load any data 
 
- window.Uppy.pick({
-  target: 'body',
-  maxNumberOfFiles: 1,
-  params: {
-    auth: { key: context.keys['AUTH KEY'] },
-    template_id: context.keys['TEMPLATE ID']
-  },
-   waitForEncoding:true
-})
-.then((bundle) => {
-}).then(res=>res.json())
-  .then(res => console.log(res));
-   
-  //bundle.transloadit // Array of Assembly statuses
-  //bundle.results // Array of all Assembly results
+    window.Uppy.pick({
+        target: 'body',
+        maxNumberOfFiles: 1,
+        params: {
+            auth: { key: context.keys['AUTH KEY'] },
+            template_id: context.keys['TEMPLATE ID']
+        },
+        waitForEncoding: true
+    })
+        .then((bundle) => {
+            fetch(context.keys['NOTIFY URL'], {
+                method: 'post',
+                headers: {
+                    'Accept': 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(bundle.transloadit)
+            }).then(res => res.json())
+                .then(res => console.log(res));
+
+        })
 }
